a2db3615009a0bf97947d17dd59e4c48932ab800
anilyavas <anil.yavas99@gmail.com>
2025-02-14 19:35:11 +0300

    Initialize project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,10 @@ import '../global.css';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="index" />
+    </Stack>
+  );
 }
---
+++ app/welcome.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function WelcomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-yellow-950">
+      <Text className="text-4xl font-bold text-white">Welcome to the application</Text>
+    </View>
+  );
+}
