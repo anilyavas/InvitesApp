5609a4c601f24a2a1e4d9f59da652a7ddb4c9a7c
anilyavas <anil.yavas99@gmail.com>
2025-02-16 15:43:38 +0300

    Welcome screen flex layout

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,9 +1,21 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function WelcomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-yellow-950">
-      <Text className="text-4xl font-bold text-white">Welcome to the application</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center bg-yellow-950">
+      <View className="h-[60%] w-full bg-gray-400">{/* Marquee comp */}</View>
+
+      <View className="flex-1 justify-center gap-4 p-4">
+        <Text className="text-center text-2xl font-bold text-white/60">Welcome to</Text>
+        <Text className="text-center text-5xl font-bold text-white">Apple Invites</Text>
+        <Text className="mb-5 text-center text-lg text-white/60">
+          Create beautiful invitation for your events. Anyone can receive your invitations.
+        </Text>
+        <Pressable className="items-center self-center rounded-full bg-white px-10 py-4">
+          <Text className="text-lg">Create an Event</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
